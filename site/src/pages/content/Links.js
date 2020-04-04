7fb5bc9a344d4a1de5eafcd4a1fff484e0745dc1
eteeeeeerminal <momentneet@gmail.com>
2020-04-05 00:52:14 +0900

    links と works のファイルを作った

--- site/src/pages/content/Links.js
+++ site/src/pages/content/Links.js
@@ -4,14 +4,14 @@ import Layout from "../../components/layout/layout"
 import GetDataJson from "../../utils/getDataJson"
 import Image from "../../components/image"
 
-//import "../../styles/Works.css"
+//import "../../styles/links.css"
 
-const WorksPage = () => {
+const LinksPage = () => {
   return (
-    <Layout title="Works" topPoem="さくひん" bottomPoem="届くといいな">
+    <Layout title="Links" topPoem="私というつながり" bottomPoem="旅のはじまり">
       準備中みたい...
     </Layout>
   )
 }
 
-export default WorksPage
+export default LinksPage
