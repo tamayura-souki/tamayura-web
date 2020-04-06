ed977cda1400c184c05c4c41fcc019dabd2ba930
eteeeeeerminal <momentneet@gmail.com>
2020-04-06 15:23:52 +0900

    works 仮完成

--- site/src/pages/content/Works.js
+++ site/src/pages/content/Works.js
@@ -4,12 +4,48 @@ import Layout from "../../components/layout/layout"
 import GetDataJson from "../../utils/getDataJson"
 import Image from "../../components/image"
 
-//import "../../styles/Works.css"
+import "../../styles/works.css"
+
+const worksList = props => {
+  const worksItems = props.contents.map((data, i) => {
+    var image = (<div className="worksImage"><Image filename={data.image}/></div>);
+
+    var item = (
+      <>
+        {image}
+        <h2>{data.name}</h2>
+        <p>{data.description}</p>
+      </>
+    )
+
+    if(data.link) {
+      item = (<a href={data.link}>{item}</a>)
+    }
+
+    return (
+      <div className="worksItem" key={"worksItem"+i}>
+        {item}
+      </div>
+    )
+  })
+
+  return (
+    <div className="worksList">
+      <h1>{props.caption}</h1>
+      <p>{props.detail}</p>
+      {worksItems}
+    </div>
+  );
+}
 
 const WorksPage = () => {
+  const worksLists = GetDataJson("works").map((data, i) => (
+    <div key={"worksList"+i}>{worksList(data)}</div>
+  ))
+
   return (
     <Layout title="Works" topPoem="さくひん" bottomPoem="届くといいな">
-      準備中みたい...
+      <div id="worksLists">{worksLists}</div>
     </Layout>
   )
 }
