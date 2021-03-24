d148be649d22e898d2ed93874240329a331f463c
eteeeeeerminal <momentneet@gmail.com>
2021-03-24 21:03:18 +0900

    リファクタ

--- site/src/components/image.js
+++ site/src/components/image.js
@@ -1,17 +1,13 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import {graphql, useStaticQuery } from 'gatsby'
 import {GatsbyImage, getImage} from 'gatsby-plugin-image'
 
 // 参考: https://takumon.com/simple-gatsby-image-wrapper
 // 画像ファイルパスをプロパティに取るようなコンポーネントを定義
-const Image = ({ filename }) => (
-
-  // ページじゃないコンポーネントでもGraphQLが使えるように
-  // StaticQueryタグを使う
-  <StaticQuery
-
-    // GraphQLのクエリ引数には何も指定しない！
-    query={graphql`
+const Image = ({ filename }) => {
+  // 全画像情報がdataに代入されている
+  const { images } =  useStaticQuery(
+    graphql`
       query {
         images: allFile {
           edges {
@@ -25,24 +21,19 @@ const Image = ({ filename }) => (
           }
         }
       }
-    `}
+    `)
 
-    // 全画像情報がdataに代入されている
-    render={(data) => {
-
-      // 指定した画像ファイルパス（コンポーネントのプロパティ）と
-      // 一致するgatsby-image用の情報を取得
-      const image_data = data.images.edges.find(n => {
-        return n.node.relativePath.includes(filename)
-      })
+    // 指定した画像ファイルパス（コンポーネントのプロパティ）と
+    // 一致するgatsby-image用の情報を取得
+    const image_data = images.edges.find(n => {
+      return n.node.relativePath.includes(filename)
+    })
 
 
-      if (!image_data) return
-      const image = getImage(image_data.node)
-      if (!image) return
-      return <GatsbyImage image={image} alt="lost" />
-    }}
-  />
-)
+    if (!image_data) return
+    const image = getImage(image_data.node)
+    if (!image) return
+    return <GatsbyImage image={image} alt="lost" />
+}
 
 export default Image
